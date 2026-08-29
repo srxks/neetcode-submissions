a55ee8354c5e0d080f42b57e47692d59b93a096f
Srikant Subramanian <srikantvenkatsubramanian@gmail.com>
2026-08-29 09:52:57 +0530

    Update: is-palindrome - submission-2

--- Data Structures & Algorithms/is-palindrome/submission-2.py
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -1,10 +1,14 @@
 class Solution:
-    def isValid(self, ch: chr) -> bool:
-        return (ord('a') <= ord(ch) <= ord('z') or
-            ord('A') <= ord(ch) <= ord('Z') or
-            ord('0') <= ord(ch) <= ord('9'))
-
     def isPalindrome(self, s: str) -> bool:
-        stripped = ''.join([ch.lower() for ch in s if self.isValid(ch)])
-        print(stripped)
-        return (stripped[::-1]) == stripped
+        k = [x.lower() for x in s if (x != " " and x.isalnum())]
+        l, f = 0, len(k)-1
+        while l < f:
+            print(k[l], k[f])
+            if k[l] != k[f]:
+                return False
+            
+            l += 1
+            f -= 1
+
+
+        return True
